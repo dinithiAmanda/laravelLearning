8d7c18e1bb917fbd0a903f6e2535d1d9a46f8149
Dinithi Amanda <amandadinithi00@gmail.com>
2024-10-11 13:15:17 +0530

    dami add

---
+++ laravel_crud_oparetion/resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="footer">
+    <p>copyright &copy; <a href="">mywebsite</a> 2024</p>
+</div>
---
+++ laravel_crud_oparetion/resources/views/pages/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('pageContent')
+<div class="page-content">
+    <p>home</p>
+</div>
+@endsection
